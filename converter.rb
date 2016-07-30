ebaa180ddf79c7f602dc17c5decd0cc283091d56
Frank Matranga <thefrankmatranga@gmail.com>
2016-07-30 17:45:22 -0400

    Ruby XML to sqlite3 converter

---
+++ converter.rb
@@ -0,0 +1,37 @@
+require 'sqlite3'
+require 'nokogiri'
+
+VOCAB_DIR = './dict/'.freeze
+
+# Open the database
+db = SQLite3::Database.new 'data.db'
+
+db.execute('DROP TABLE vocabulary')
+
+# Create a table
+db.execute <<-SQL
+  CREATE TABLE IF NOT EXISTS vocabulary (
+    id INTEGER PRIMARY KEY,
+    chapter INTEGER,
+    type VARCHAR(10),
+    latin VARCHAR(20),
+    english VARCHAR(70),
+    gender VARCHAR(5)
+  );
+SQL
+
+(1..40).each do |chapter|
+  puts chapter
+  content = File.open(VOCAB_DIR + chapter.to_s) { |f| Nokogiri::XML(f) }
+  entries = content.xpath('//entry')
+  entries.each do |e|
+    entry_type = e.at_xpath('type').content
+    latin = e.at_xpath('latin').content
+    english = e.at_xpath('english').content
+    gender = e.at_xpath('gender')
+
+    gender = gender.nil? ? nil : gender.content
+
+    db.execute('INSERT INTO vocabulary (chapter, type, latin, english, gender) VALUES (?, ?, ?, ?, ?)', [chapter, entry_type, latin, english, gender])
+  end
+end
